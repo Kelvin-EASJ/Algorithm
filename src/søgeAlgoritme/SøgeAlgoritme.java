ce8818eff6f493078375dba069ca95f18ee74e62
Kelvin-EASJ <kelvin.aggerholm@gmail.com>
2020-03-16 09:30:35 +0100

    tilføjet kommentar

--- src/søgeAlgoritme/SøgeAlgoritme.java
+++ src/søgeAlgoritme/SøgeAlgoritme.java
@@ -3,8 +3,8 @@ package søgeAlgoritme;
 public class SøgeAlgoritme {
 
     public static void main(String[] args) {
-        int[] data = {3, 5, 20, 7, 10, 8, 12};
-        int find8 = 8;
+        int[] data = {3, 5, 20, 7, 10, 8, 12}; //Arraylisten hvor index for 8 findes
+        int find8 = 8; //funktionen der specificere index
 
         for(int i = 0; i<data.length; i++) {
             if (find8 == data[i]) {
